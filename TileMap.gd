1a4bc46df953445227463103ce8e21000d9845d4
Enemy-Stand <sestijoey63@gmail.com>
2024-04-26 23:14:06 -0500

    spawned bricks

--- TileMap.gd
+++ TileMap.gd
@@ -1,25 +1,25 @@
-extends Node2D
+extends TileMap
 var brick_scene: PackedScene = preload("res://Brick/brick.tscn")
 var tile_grid = [
-	[1,1,1,1,1,1,1,1,1,1,1,1],
-	[1,1,1,1,1,1,1,1,1,1,1,1],
-	[1,1,1,1,1,1,1,1,1,1,1,1],
-	[1,1,1,1,1,1,1,1,1,1,1,1],
-	[1,1,1,1,1,1,1,1,1,1,1,1],
+	[1,1,1,1,1,1,1,1],
+	[1,1,1,1,1,1,1,1],
+	[1,1,1,1,1,1,1,1],
+	[1,1,1,1,1,1,1,1]
 ]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	fill_grid()
-	pass
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
 
 func fill_grid():
 	for i in range(tile_grid.size()):
 		for j in range(tile_grid[i].size()):
-			var brick = brick_scene.instantiate()
-			brick.global_position = Vector2((j * 64) + 220, (i * 48) + 90)
-			add_child(brick)
+			tile_map.get_cell_tile_data(i, j)
+			#var brick = brick_scene.instantiate()
+			#brick.global_position = Vector2(i * 32, j * 16)
+			#add_child(brick)
